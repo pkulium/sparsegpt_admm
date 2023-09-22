e27e6dfe719515fcd2d68804f40558435e94c00d
mliu <pkulium@gmail.com>
2023-09-21 20:13:28 -0500

    Please enter the commit message for your changes. Lines starting with '#' will be ignored, and an empty message aborts the commit.
    On branch master
    Your branch is up to date with 'origin/master'.
    
    Changes to be committed:
    	modified:   sparsegpt.py

--- sparsegpt.py
+++ sparsegpt.py
@@ -169,6 +169,7 @@ class SparseGPT:
         model = nn.Linear(in_features, out_features)  # Create a new linear model with the same specifications
         model = model.to(self.dev)
         model.weight.data = self.layer.weight.data.clone()  # Copy the weights from the old model to the new model
+        model.weight.data = model.weight.data.dtype(torch.float16)
         self.lr = 1e-3
         self.adam_epsilon = 1e-8
         self.alpha = 5e-4
