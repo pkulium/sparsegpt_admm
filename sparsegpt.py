67e847aefb5de396a9db13db7af24db4d782d697
mliu <pkulium@gmail.com>
2023-09-22 11:06:43 -0500

    Please enter the commit message for your changes. Lines starting with '#' will be ignored, and an empty message aborts the commit.
    On branch master
    Your branch is up to date with 'origin/master'.
    
    Changes to be committed:
    	modified:   sparsegpt.py

--- sparsegpt.py
+++ sparsegpt.py
@@ -177,6 +177,7 @@ class SparseGPT:
         optimizer = PruneAdam(model.named_parameters(), lr=self.lr, eps=self.adam_epsilon)
         self.l1 = False
         self.l2 = False
+        self.percent = [0.8, 0.92, 0.991, 0.93]
         train(self, model, self.dev, self.inp1.clone(), self.out1.clone(), optimizer)
         mask = apply_l1_prune(model, self.dev, self) if self.l1 else apply_prune(model, self.dev, self)
         print_prune(model)
