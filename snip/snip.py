1ac5a0ab607d36c5ab238d5dcc9c577ba141600b
mliu <pkulium@gmail.com>
2023-09-27 12:45:57 -0500

    Please enter the commit message for your changes. Lines starting with '#' will be ignored, and an empty message aborts the commit.
    On branch master
    Your branch is up to date with 'origin/master'.
    
    Changes to be committed:
    	modified:   snip/snip.py

--- snip/snip.py
+++ snip/snip.py
@@ -116,7 +116,7 @@ def PGD(net, keep_ratio, train_dataloader, device):
     num_params_to_keep = int(net.weight_mask.shape[0] * net.weight_mask.shape[1] * keep_ratio)
     threshold, _ = torch.topk(torch.flatten(net.weight_mask), num_params_to_keep, sorted=True)
     acceptable_score = threshold[-1]
-
+    print(f'shape {net.weight_mask}:{acceptable_score.shape}')
     keep_masks = net.weight_mask > acceptable_score
     return (keep_masks)
 
