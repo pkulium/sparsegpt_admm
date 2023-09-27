607376eea41148a689551fff2c6068e13069dd00
mliu <pkulium@gmail.com>
2023-09-27 16:37:48 -0500

    Please enter the commit message for your changes. Lines starting with '#' will be ignored, and an empty message aborts the commit.
    On branch master
    Your branch is up to date with 'origin/master'.
    
    Changes to be committed:
    	modified:   snip/snip.py

--- snip/snip.py
+++ snip/snip.py
@@ -117,7 +117,7 @@ def PGD(net, keep_ratio, train_dataloader, device):
     criterion = nn.MSELoss()  # Mean Squared Error Loss for regression
     # mask_optimizer = torch.optim.SGD([net.weight_mask], lr=0.001, momentum=0.9)
     mask_optimizer = torch.optim.Adam([net.weight_mask], lr=0.01)
-    for epoch in range(1000):
+    for epoch in range(200):
         for i, (inputs, targets) in enumerate(train_dataloader):
             inputs, targets = inputs.to(device), targets.to(device)
 
