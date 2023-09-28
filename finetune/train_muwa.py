3837d4f3880a18e76e297c3c3198ed082a2f602f
mliu <pkulium@gmail.com>
2023-09-28 17:36:38 -0500

    Please enter the commit message for your changes. Lines starting with '#' will be ignored, and an empty message aborts the commit.
    On branch master
    Your branch is up to date with 'origin/master'.
    
    Changes to be committed:
    	modified:   finetune/train_muwa.py

--- finetune/train_muwa.py
+++ finetune/train_muwa.py
@@ -59,28 +59,32 @@ def masked_forward_linear(self, x: torch.Tensor):
         return F.linear(x, T(self.weight), bias=self.bias)
     
 def masked_forward_linear(self, x: torch.Tensor) -> torch.Tensor:
-    previous_dtype = x.dtype
+    if self.active_adapter not in self.lora_A.keys():
+        return self._linear(x)
 
+    previous_dtype = x.dtype
     if self.disable_adapters:
-        if self.merged:
+        if (self.r[self.active_adapter] > 0) and self.merged:
             self.unmerge()
         result = self._linear(x)
-    elif self.merged:
+    elif (self.r[self.active_adapter] == 0) or self.merged:
         result = self._linear(x)
     else:
-        result = self._linear(x)
-        for active_adapter in self.active_adapters:
-            if active_adapter not in self.lora_A.keys():
-                continue
-            lora_A = self.lora_A[active_adapter]
-            lora_B = self.lora_B[active_adapter]
-            dropout = self.lora_dropout[active_adapter]
-            scaling = self.scaling[active_adapter]
-            x = x.to(lora_A.weight.dtype)
-            tmp = self.mask * (lora_A.transpose(0, 1) @ lora_B.transpose(0, 1))
-            result += (dropout(x) @ tmp) * self.scaling
-            result += lora_B(lora_A(dropout(x))) * scaling
+        lora_A = self.lora_A[self.active_adapter]
+        lora_B = self.lora_B[self.active_adapter]
+        dropout = self.lora_dropout[self.active_adapter]
+        scaling = self.scaling[self.active_adapter]
 
+        result = self._linear(x)
+        x = x.to(lora_A.weight.dtype)
+        # result += lora_B(lora_A(dropout(x))) * scaling
+        tmp = self.mask * (lora_A.transpose(0, 1) @ lora_B.transpose(0, 1))
+        result += (dropout(x) @ tmp) * self.scaling
+
+    result = result.to(previous_dtype)
+    return result
+
+           
     result = result.to(previous_dtype)
     return result
 
