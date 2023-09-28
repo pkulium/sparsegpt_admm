7d2d454c88e5b9aea887499e86471b01c2c1637b
mliu <pkulium@gmail.com>
2023-09-28 17:29:38 -0500

    Please enter the commit message for your changes. Lines starting with '#' will be ignored, and an empty message aborts the commit.
    On branch master
    Your branch is up to date with 'origin/master'.
    
    Changes to be committed:
    	modified:   finetune/train_muwa.py

--- finetune/train_muwa.py
+++ finetune/train_muwa.py
@@ -57,6 +57,32 @@ def masked_forward_linear(self, x: torch.Tensor):
         return result
     else:
         return F.linear(x, T(self.weight), bias=self.bias)
+    
+def masked_forward_linear(self, x: torch.Tensor) -> torch.Tensor:
+    previous_dtype = x.dtype
+
+    if self.disable_adapters:
+        if self.merged:
+            self.unmerge()
+        result = self._linear(x)
+    elif self.merged:
+        result = self._linear(x)
+    else:
+        result = self._linear(x)
+        for active_adapter in self.active_adapters:
+            if active_adapter not in self.lora_A.keys():
+                continue
+            lora_A = self.lora_A[active_adapter]
+            lora_B = self.lora_B[active_adapter]
+            dropout = self.lora_dropout[active_adapter]
+            scaling = self.scaling[active_adapter]
+            x = x.to(lora_A.weight.dtype)
+            tmp = self.mask * (lora_A.transpose(0, 1) @ lora_B.transpose(0, 1))
+            result += (dropout(x) @ tmp) * self.scaling
+            result += lora_B(lora_A(dropout(x))) * scaling
+
+    result = result.to(previous_dtype)
+    return result
 
 def add_masked_layers(model):
     for name, module in model.named_modules():
