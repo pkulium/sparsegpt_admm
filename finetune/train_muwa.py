387b7518c10ac85bf93a062f79d2ca2c7049a009
mliu <pkulium@gmail.com>
2023-09-29 16:41:14 -0500

    Please enter the commit message for your changes. Lines starting with '#' will be ignored, and an empty message aborts the commit.
    On branch master
    Your branch is up to date with 'origin/master'.
    
    Changes to be committed:
    	modified:   finetune/train_muwa.py

--- finetune/train_muwa.py
+++ finetune/train_muwa.py
@@ -139,10 +139,10 @@ class ADMMCallback(TrainerCallback):
         # If you need to access or modify model parameters, optimizer, etc.
         # You can access them using the `model` and `trainer` objects
         # For example: model.parameters(), trainer.optimizer, etc.
-        # clip_mask(model)
-        self.update_X()
-        self.update_Z()
-        self.update_U()
+        clip_mask(model)
+        # self.update_X()
+        # self.update_Z()
+        # self.update_U()
         
     def on_epoch_end(self, args, state, control, model=None, **kwargs):
         # This will be executed at the end of each epoch
