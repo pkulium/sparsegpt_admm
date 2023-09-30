92f53d8dc105c3c25e4f2738cb00c48760df541e
mliu <pkulium@gmail.com>
2023-09-30 10:22:11 -0500

    Please enter the commit message for your changes. Lines starting with '#' will be ignored, and an empty message aborts the commit.
    On branch master
    Your branch is up to date with 'origin/master'.
    
    Changes to be committed:
    	modified:   finetune/train_muwa.py

--- finetune/train_muwa.py
+++ finetune/train_muwa.py
@@ -79,7 +79,7 @@ def masked_forward_linear(self, x: torch.Tensor) -> torch.Tensor:
 def add_masked_layers(model):
     for name, module in model.named_modules():
         if 'q_proj' in name[-6:] or 'v_proj' in name[-6:]:
-            module.lora_mask = nn.Parameter(torch.ones_like(module.weight).to(module.weight.dtype))
+            module.lora_mask = nn.Parameter(torch.ones_like(module.weight).to(module.weight.dtype) * 0.5)
             module.lora_mask.requires_grad = True
             module.prun_mask = nn.Parameter(torch.ones_like(module.weight).to(module.weight.dtype))
             # Modify forward method
