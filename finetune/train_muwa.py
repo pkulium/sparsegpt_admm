d888e175dec8a837939d6438269c54f6919528bb
mliu <pkulium@gmail.com>
2023-10-04 15:06:24 -0500

    Please enter the commit message for your changes. Lines starting with '#' will be ignored, and an empty message aborts the commit.
    On branch master
    Your branch is up to date with 'origin/master'.
    
    Changes to be committed:
    	modified:   finetune/train_muwa.py

--- finetune/train_muwa.py
+++ finetune/train_muwa.py
@@ -251,15 +251,16 @@ class CustomTrainer(Trainer):
             # We don't use .loss here since the model may return tuples instead of ModelOutput.
             loss = outputs["loss"] if isinstance(outputs, dict) else outputs[0]
 
-        # print(f'loss nature {loss}')
-        admm_loss = 0
-        for name, mask in self.admm.ADMM_X.items():
-            admm_loss = self.admm.rho[name] / 2 * (self.admm.ADMM_X[name] - self.admm.ADMM_U[name]).norm()
-            admm_loss += admm_loss
-            # if name == 'base_model.model.model.decoder.layers.0.self_attn.v_proj':
-                # print(f'loss:{self.admm.ADMM_U[name]}')
-        loss += admm_loss
-        print(f'loss admm {admm_loss}')
+        if self.train_mask:
+            # print(f'loss nature {loss}')
+            admm_loss = 0
+            for name, mask in self.admm.ADMM_X.items():
+                admm_loss = self.admm.rho[name] / 2 * (self.admm.ADMM_X[name] - self.admm.ADMM_U[name]).norm()
+                admm_loss += admm_loss
+                # if name == 'base_model.model.model.decoder.layers.0.self_attn.v_proj':
+                    # print(f'loss:{self.admm.ADMM_U[name]}')
+            loss += admm_loss
+            print(f'loss admm {admm_loss}')
         return (loss, outputs) if return_outputs else loss
 
 def switch(model):
