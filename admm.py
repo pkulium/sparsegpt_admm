f40615b2c04d29463e120cfde818ac55f20f930d
mliu <pkulium@gmail.com>
2023-10-06 15:31:52 -0500

    Please enter the commit message for your changes. Lines starting with '#' will be ignored, and an empty message aborts the commit.
    On branch master
    Your branch is up to date with 'origin/master'.
    
    Changes to be committed:
    	modified:   .DS_Store
    	modified:   admm.py

--- admm.py
+++ admm.py
@@ -53,7 +53,7 @@ class ADMM:
             if 'q_proj' in name[-6:] or 'v_proj' in name[-6:]:
                 self.rho[name] = 0.01
                 _, m = get_n_m_sparse_matrix(torch.rand_like(module.prun_mask))
-                self.ADMM_U[name] = m.data.to(dtype = module.weight.dtype, device = module.device)
+                self.ADMM_U[name] = m.data.to(dtype = module.weight.dtype, device = module.prun_mask.device)
                 self.ADMM_U[name].requires_grad = False
 
 
