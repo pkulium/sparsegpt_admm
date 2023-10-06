66656df9a8b3223d638da99c64a62518b8266013
mliu <pkulium@gmail.com>
2023-10-06 16:18:47 -0500

    Please enter the commit message for your changes. Lines starting with '#' will be ignored, and an empty message aborts the commit.
    On branch master
    Your branch is up to date with 'origin/master'.
    
    Changes to be committed:
    	modified:   train_muwa.py

--- train_muwa.py
+++ train_muwa.py
@@ -146,7 +146,7 @@ class ADMMCallback(TrainerCallback):
         dataloader, testloader = get_loaders(
             'c4', nsamples=config.nsamples, seed=config.seed, model=model, seqlen=model.seqlen, tokenizer = tokenizer
         )
-        opt_sequential(model.model, dataloader, dev=config.device)  
+        opt_sequential(model.model, dataloader, dev=config.device, model0 = model)  
 
     def update_U(self, args, state, control, model=None, **kwargs):
         for name, module in model.named_modules():
@@ -221,7 +221,7 @@ def custom_optimizer(model):
     return optimizer 
 
 @torch.no_grad()
-def opt_sequential(model, dataloader, dev):
+def opt_sequential(model, dataloader, dev, model0):
     print('Starting ...')
 
     use_cache = model.config.use_cache
