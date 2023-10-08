338e7be2ab9dfc6f4ac2b92c04472ef8cd46de8b
mliu <pkulium@gmail.com>
2023-10-08 15:32:12 -0500

    Please enter the commit message for your changes. Lines starting with '#' will be ignored, and an empty message aborts the commit.
    On branch master
    Your branch is up to date with 'origin/master'.
    
    Changes to be committed:
    	modified:   train_muwa.py

--- train_muwa.py
+++ train_muwa.py
@@ -262,6 +262,7 @@ def pgd_prun_mask(module, module_name, admm):
     module.prun_mask.requires_grad = True
 
     inputs = module.last_input
+    inputs = inputs.to(model.weight.dtype)
     targets = module.last_expected_output
 
     from torch.utils.data import TensorDataset, DataLoader
@@ -280,7 +281,7 @@ def pgd_prun_mask(module, module_name, admm):
         mask_optimizer.zero_grad()
         outputs = model.forward(inputs)
         loss = criterion(outputs, targets)  # Compute the loss
-        l1_reg = rho / 2 * (model.prun_mask - model.lora_mask + admm.ADMM_U[module_name]).norm()
+        l1_reg = rho / 2 * model.prun_mask - model.lora_mask + admm.ADMM_U[module_name]).norm()
         loss += l1_reg
         loss.backward()
         mask_optimizer.step()
