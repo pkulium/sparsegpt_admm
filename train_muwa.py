50ff9450da3a6728129e848241010562292e88ed
pkulium <54947136+pkulium@users.noreply.github.com>
2023-10-12 13:35:53 -0500

    Please enter the commit message for your changes. Lines starting with '#' will be ignored, and an empty message aborts the commit.
    On branch master
    Your branch is up to date with 'origin/master'.
    
    Changes to be committed:
    	modified:   train_muwa.py
    
    Changes not staged for commit:
    	modified:   train_muwa.py

--- train_muwa.py
+++ train_muwa.py
@@ -251,8 +251,8 @@ def clip_mask(model, lower=0.0, upper=1.0):
     with torch.no_grad():
         for param in params:
             param.clamp_(lower, upper)
-            w, m = get_n_m_sparse_matrix(param)
-            param.data = m.to(param.dtype)
+            # w, m = get_n_m_sparse_matrix(param)
+            # param.data = m.to(param.dtype)
 
 def custom_optimizer(model):
     # Access the model's parameters
