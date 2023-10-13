076c75ec995dffcd3f3c975c739a100100972498
pkulium <54947136+pkulium@users.noreply.github.com>
2023-10-13 11:23:19 -0500

    Please enter the commit message for your changes. Lines starting with '#' will be ignored, and an empty message aborts the commit.
    On branch master
    Your branch is up to date with 'origin/master'.
    
    Changes to be committed:
    	modified:   train_muwa.py

--- train_muwa.py
+++ train_muwa.py
@@ -470,7 +470,7 @@ if __name__ == '__main__':
 
     parser.add_argument(
         '--model', type=str, 
-        default = 'facebook/opt-13b',
+        default = 'facebook/opt-1.3b',
         help='OPT model to load; pass `facebook/opt-X`.'
     )
     parser.add_argument(
@@ -559,7 +559,7 @@ if __name__ == '__main__':
     #         if 'fc2' in n:
     #             break
     #     print(time.time() - tick)
-    #     with open('layer_calibrations_opt_13b', 'wb') as f:
+    #     with open('layer_calibrations_opt_1.3b', 'wb') as f:
     #         pickle.dump(layer_calibrations, f)
 
     #     del model
@@ -567,16 +567,16 @@ if __name__ == '__main__':
     #     del testloader
     #     del layer_calibrations
 
-    with open('layer_calibrations_opt_13b', 'rb') as f:
+    with open('layer_calibrations_opt_1.3b', 'rb') as f:
         layer_calibrations = pickle.load(f)
 
     model = AutoModelForCausalLM.from_pretrained(
-        "facebook/opt-13b", 
+        "facebook/opt-1.3b", 
         # load_in_8bit=True, 
         device_map='auto',
     )
     
-    tokenizer = AutoTokenizer.from_pretrained("facebook/opt-13b")
+    tokenizer = AutoTokenizer.from_pretrained("facebook/opt-1.3b")
     data = load_dataset("databricks/databricks-dolly-15k")
     data = data.map(lambda samples: tokenizer(samples['instruction'], max_length=1024, truncation=True), batched=True)
 
@@ -634,7 +634,7 @@ if __name__ == '__main__':
     trainer.admm = admm
     model.config.use_cache = False 
     trainer.train(resume_from_checkpoint = False)
-    # model.save_pretrained("lora-muwa-13b-opt")
+    # model.save_pretrained("lora-muwa-1.3b-opt")
 
     model.config.pad_token_id = tokenizer.pad_token_id = 0  # unk
     model.config.bos_token_id = 1
