5bafa25b5e36fa02a1c612ca3e2d64c0f03d9450
pkulium <54947136+pkulium@users.noreply.github.com>
2023-10-18 13:47:08 -0500

    Please enter the commit message for your changes. Lines starting with '#' will be ignored, and an empty message aborts the commit.
    On branch master
    Your branch is up to date with 'origin/master'.
    
    Changes to be committed:
    	modified:   sparsegpt.py

--- sparsegpt.py
+++ sparsegpt.py
@@ -404,7 +404,7 @@ class SparseGPT:
         with torch.enable_grad():
             model.train()
             mask = PGD(model, 0.5, train_loader, self.dev)
-            print(f'shape1 {torch.sum(mask) / (model.weight_mask.shape[0] * model.weight_mask.shape[1])}')
+            # print(f'shape1 {torch.sum(mask) / (model.weight_mask.shape[0] * model.weight_mask.shape[1])}')
         self.layer.weight.data[model.weight] = 0
         del model
         del dataset
