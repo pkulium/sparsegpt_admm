c8f814a999e9947dd6dbbc3ec269dcee0b79ecc6
pkulium <54947136+pkulium@users.noreply.github.com>
2023-10-19 14:21:31 -0500

    Please enter the commit message for your changes. Lines starting with '#' will be ignored, and an empty message aborts the commit.
    On branch master
    Your branch is up to date with 'origin/master'.
    
    Changes to be committed:
    	modified:   snip/snip.py

--- snip/snip.py
+++ snip/snip.py
@@ -279,6 +279,7 @@ def VRPEG(model, keep_ratio, train_loader, device):
                 model.j = j
                 output = model(image)
                 original_loss = criterion(output, target)
+                print(f'original_loss:{original_loss}')
                 loss = original_loss/K
                 fn_list.append(loss.item()*K)
                 loss.backward(retain_graph=True)
