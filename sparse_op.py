8df8cf4478ae4df64750d544b3291cee707aa32d
pkulium <54947136+pkulium@users.noreply.github.com>
2023-10-19 14:29:14 -0500

    Please enter the commit message for your changes. Lines starting with '#' will be ignored, and an empty message aborts the commit.
    On branch master
    Your branch is up to date with 'origin/master'.
    
    Changes to be committed:
    	modified:   sparse_op.py

--- sparse_op.py
+++ sparse_op.py
@@ -237,7 +237,7 @@ class VRPGE_Linear(nn.Linear):
                     self.stored_mask_1.data = (self.subnet-self.scores)/torch.sqrt((self.scores+1e-20)*(1-self.scores+1e-20))
                 w = self.weight * self.subnet
                 print(f'w:{w}')
-                print(f'weight:{self.weight}')
+                print(f'score:{self.scores}')
                 x = F.linear(x, w, self.bias)
             else:
                 w = self.weight * self.subnet
