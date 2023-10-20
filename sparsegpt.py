08097e7d66bd8387013071d11bf4aead3d9f2443
pkulium <54947136+pkulium@users.noreply.github.com>
2023-10-20 16:35:02 -0500

    Please enter the commit message for your changes. Lines starting with '#' will be ignored, and an empty message aborts the commit.
    On branch master
    Your branch is up to date with 'origin/master'.
    
    Changes to be committed:
    	modified:   sparsegpt.py

--- sparsegpt.py
+++ sparsegpt.py
@@ -516,9 +516,9 @@ class SparseGPT:
         train_loader = DataLoader(dataset, batch_size=256, shuffle=True)
         with torch.enable_grad():
             model.train()
-            print(f'subnet:{model.subnet}')
+            print(f'orign subnet:{model.subnet}')
             VRPEG(model, 0.5, train_loader, self.dev)
-            model.fix_subnet()
+            print(f'final subnet:{model.subnet}')
             print(f'ratio:{torch.sum(model.subnet)/ model.subnet.nelement()}')
         self.layer.weight.data = model.weight.data.clone().to(dtype)
         self.layer.weight[~model.subnet.data.bool()] = 0
