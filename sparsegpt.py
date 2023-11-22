fca13e9214611f56b5fd9dd058aae32402f03eb5
pkulium <54947136+pkulium@users.noreply.github.com>
2023-11-22 16:01:56 -0600

    Please enter the commit message for your changes. Lines starting with '#' will be ignored, and an empty message aborts the commit.
    On branch master
    Your branch is up to date with 'origin/master'.
    
    Changes to be committed:
    	modified:   sparsegpt.py

--- sparsegpt.py
+++ sparsegpt.py
@@ -605,7 +605,7 @@ class SparseGPT:
         # Define your hyperparameter grids
         lr_values = [0.001, 0.01, 0.1]
         weight_lr_values = [0]
-        max_iter_values = [10, 100, 300]
+        max_iter_values = [100, 300]
 
         # Initialize variables to store the best hyperparameters and the corresponding minimum loss
         best_lr = None
