3c83ad8ec5c290979dcfc2dd26744feff75c95d6
pkulium <54947136+pkulium@users.noreply.github.com>
2023-11-22 15:21:09 -0600

    Please enter the commit message for your changes. Lines starting with '#' will be ignored, and an empty message aborts the commit.
    On branch master
    Your branch is up to date with 'origin/master'.
    
    Changes to be committed:
    	modified:   sparsegpt.py

--- sparsegpt.py
+++ sparsegpt.py
@@ -607,7 +607,7 @@ class SparseGPT:
         weight_lr_values = [0]
         max_iter_values = [10, 100]
 
-        lr_values = [0.1]
+        lr_values = [0.01]
         weight_lr_values = [0]
         max_iter_values = [300]
 
