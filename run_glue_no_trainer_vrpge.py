66aa571248f1a5e82b2a572215a6861ca2c4b11e
pkulium <54947136+pkulium@users.noreply.github.com>
2023-11-24 11:16:12 -0600

    Please enter the commit message for your changes. Lines starting with '#' will be ignored, and an empty message aborts the commit.
    On branch master
    Your branch is up to date with 'origin/master'.
    
    Changes to be committed:
    	modified:   run_glue_no_trainer_vrpge.py

--- run_glue_no_trainer_vrpge.py
+++ run_glue_no_trainer_vrpge.py
@@ -635,7 +635,7 @@ def main():
         else:
             active_dataloader = train_dataloader
         for step, batch in enumerate(active_dataloader):
-            args.K = 10
+            args.K = 2
             l = 0
             fn_list = []
             for j in range(args.K):
